3be78215f0f09af153e6c0c92b294a6f4b909966
Gal Sar Israel <83352275+GalSarIL@users.noreply.github.com>
2022-05-15 19:44:03 +0300

    Add files via upload

---
+++ website/models.py
@@ -0,0 +1,19 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Customer(db.Model):
+    id = db.Column(db.Integar, primary_key=True)
+    customerName = db.Column(db.String(150))
+    customerPackage = db.Column(db.String(150))
+    customerDOJ = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integar, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integar, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    username = db.Column(db.String(150))
+    notes = db.relationship('Customer')
