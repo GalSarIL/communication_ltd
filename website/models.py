80ef54d3eaba68b11e9bba4dd2ff74c01131a18a
Gal Sar Israel <gal.sarisrael@imperva.com>
2022-05-15 22:31:51 +0300

    Added DB to the app

--- website/models.py
+++ website/models.py
@@ -4,15 +4,15 @@ from sqlalchemy.sql import func
 
 
 class Customer(db.Model):
-    id = db.Column(db.Integar, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     customerName = db.Column(db.String(150))
     customerPackage = db.Column(db.String(150))
     customerDOJ = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integar, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integar, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     username = db.Column(db.String(150))
